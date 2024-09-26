f4eec42f7350f24f0ba1361c3c5be3b5337f6b48
dungle <quangdung100194@gmail.com>
2024-09-26 10:15:16 +0700

    feat: ton connect test app

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'docs'
-  },
+  // build: {
+  //   outDir: 'docs'
+  // },
   // @ts-ignore
-  base: process.env.GH_PAGES ? '/demo-dapp-with-wallet/' : './',
+  // base: process.env.GH_PAGES ? '/demo-dapp-with-wallet/' : './',
   server: {
     fs: {
       allow: ['../sdk', './'],

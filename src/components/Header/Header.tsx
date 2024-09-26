d3bb29f5b6f1ceb019605c8e87f1aa39659840d0
dungle <quangdung100194@gmail.com>
2024-09-26 13:33:48 +0700

    feat: ton connect bridge api

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,20 @@ import {TonConnectButton} from "@tonconnect/ui-react";
 import './header.scss';
 
 export const Header = () => {
+    const isUseTonBridge = localStorage.getItem('useTonBridge');
     return <header>
         <span>My App with React UI</span>
         <button onClick={()=> {
-            localStorage.setItem("useTonBridge", '1')
-        }}>Use Ton Bridge</button>
+            if(isUseTonBridge) {
+                localStorage.removeItem("useTonBridge");
+
+            }
+            else {
+                localStorage.setItem("useTonBridge", '1');
+
+            }
+            window.location.reload();
+        }}>{isUseTonBridge ? 'Use Tobi Bridge' : 'Use Ton Bridge'}</button>
         <TonConnectButton />
     </header>
 }
